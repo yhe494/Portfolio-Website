e0443bee2adfaf8dc9399e0566a05309aacc393d
Yanhua He <yhe494@gmail.com>
2026-02-24 20:35:59 -0500

    change viewport name

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,9 @@
 import "./globals.css"
+import type { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Yanhua He | Portfolio",
+}
 
 export default function RootLayout({
   children,
